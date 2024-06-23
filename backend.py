c9f96ce70bfb46fbddaeeccfb0176e1e31f44d1d
Jspeedman <160780053+Jspeedman@users.noreply.github.com>
2024-06-23 20:33:39 +0200

    Update requirements.txt

--- backend.py
+++ backend.py
@@ -1,16 +1,13 @@
 # %% [markdown]
-# # Simulindex
+# # Telemindex
 
 # %% [markdown]
 # ## Importamos librerías
 
 # %%
-#En pruebas: Uso de la variables mes_seleccionado definida en el archivo 'globals.py'
-
-# %%
 import pandas as pd
 import plotly.express as px
-#import globals
+
 
 # %% [markdown]
 # ## Importamos el fichero excel
@@ -37,22 +34,13 @@ df_hist['precio_6.1']=round(df_hist['precio_6.1']/10,1)
 df_hist['spot']=round(df_hist['spot'],2)
 df_hist
 
-# %% [markdown]
-# graf_hist=px.scatter(df_hist,x='spot',y=['precio_2.0','precio_3.0','precio_6.1'], trendline='ols',
-#                 labels={'value':'c€/kWh','variable':'Precios s/ ATR','spot':'Precio medio mercado mayorista €/MWh'},
-#                 height=500,
-#                 
-#                 )
-#     
-# 
-#   
-# 
-
 # %%
 def graf_hist(omip):
+    colores_precios = {'precio_2.0': 'goldenrod', 'precio_3.0': 'darkred', 'precio_6.1': 'blue'}
     graf_hist=px.scatter(df_hist,x='spot',y=['precio_2.0','precio_3.0','precio_6.1'], trendline='ols',
                 labels={'value':'c€/kWh','variable':'Precios s/ ATR','spot':'Precio medio mercado mayorista €/MWh'},
                 height=500,
+                color_discrete_map=colores_precios
     )
     
     trend_results = px.get_trendline_results(graf_hist)
@@ -73,17 +61,17 @@ def graf_hist(omip):
     simul_61=round(intercept_61+slope_61*omip,1)
 
     graf_hist.add_scatter(x=[omip],y=[simul_20], mode='markers', 
-        marker=dict(color='rgba(255, 255, 255, 0)',size=20, line=dict(width=5, color='blue')),
+        marker=dict(color='rgba(255, 255, 255, 0)',size=20, line=dict(width=5, color='goldenrod')),
         name='Simul 2.0',
         text='Simul 2.0'
     )
     graf_hist.add_scatter(x=[omip],y=[simul_30], mode='markers', 
-        marker=dict(color='rgba(255, 255, 255, 0)',size=20, line=dict(width=5, color='red')),
+        marker=dict(color='rgba(255, 255, 255, 0)',size=20, line=dict(width=5, color='darkred')),
         name='Simul 3.0',
         text='Simul 3.0'
     )
     graf_hist.add_scatter(x=[omip],y=[simul_61], mode='markers', 
-        marker=dict(color='rgba(255, 255, 255, 0)',size=20, line=dict(width=5, color='green')),
+        marker=dict(color='rgba(255, 255, 255, 0)',size=20, line=dict(width=5, color='blue')),
         name='Simul 6.1',
         text='Simul 6.1'
     )
@@ -96,64 +84,6 @@ def graf_hist(omip):
         line=dict(color='grey', width=1,dash='dash'),
     )
     return graf_hist,simul_20,simul_30,simul_61
-
-# %% [markdown]
-# #graf_hist()
-# graf_hist()
-# 
-
-# %% [markdown]
-# omip=50
-
-# %% [markdown]
-# trend_results = px.get_trendline_results(graf_hist())
-# trend_results
-
-# %% [markdown]
-# #obtención del precio 2.0 simulado a partir del gráfico de tendencia 2.0
-# params_20 = trend_results[trend_results['Precios s/ ATR']=='precio_2.0'].px_fit_results.iloc[0].params
-# intercept_20, slope_20 = params_20[0], params_20[1]
-# simul_20=intercept_20+slope_20*omip
-
-# %% [markdown]
-# #obtención del precio 3.0 simulado a partir del gráfico de tendencia 3.0
-# params_30 = trend_results[trend_results['Precios s/ ATR']=='precio_3.0'].px_fit_results.iloc[0].params
-# intercept_30, slope_30 = params_30[0], params_30[1]
-# simul_30=intercept_30+slope_30*omip
-# 
-
-# %% [markdown]
-# #obtención del precio 6.1 simulado a partir del gráfico de tendencia 6.1
-# params_61 = trend_results[trend_results['Precios s/ ATR']=='precio_6.1'].px_fit_results.iloc[0].params
-# intercept_61, slope_61 = params_61[0], params_61[1]
-# simul_61=round(intercept_61+slope_61*omip,1)
-
-# %% [markdown]
-# graf_hist.add_scatter(x=[omip],y=[simul_20], mode='markers', 
-#     marker=dict(color='rgba(255, 255, 255, 0)',size=20, line=dict(width=5, color='blue')),
-#     name='Simul 2.0',
-#     text='Simul 2.0'
-# )
-# graf_hist.add_scatter(x=[omip],y=[simul_30], mode='markers', 
-#     marker=dict(color='rgba(255, 255, 255, 0)',size=20, line=dict(width=5, color='red')),
-#     name='Simul 3.0',
-#     text='Simul 3.0'
-# )
-# graf_hist.add_scatter(x=[omip],y=[simul_61], mode='markers', 
-#     marker=dict(color='rgba(255, 255, 255, 0)',size=20, line=dict(width=5, color='green')),
-#     name='Simul 6.1',
-#     text='Simul 6.1'
-# )
-# graf_hist.add_shape(
-#     type='line',
-#     x0=omip,
-#     y0=0,
-#     x1=omip,
-#     y1=simul_20,
-#     line=dict(color='grey', width=1,dash='dash'),
-#     
-# )
-# graf_hist
 
 # %% [markdown]
 # ### Interacción en streamlit: Listado de meses disponibles para usar en un select_box
@@ -188,24 +118,18 @@ lista_meses
 # %%
 ### Inicializamos dffm, que es el la tabla filtrada por el usuario
 
-# %% [markdown]
-# def aplicar_margen(mes_seleccionado,margen_aplicado):
-#     #dffa_copia=dffa
-#     dffa_copia['precio_2.0']=dffa['precio_2.0'] #+=margen_aplicado #=dffm['precio_2.0']+margen
-#     dffa_copia['precio_3.0']=dffa['precio_3.0'] #+=margen_aplicado #dffm['precio_3.0']+margen
-#     dffa_copia['precio_6.1']=dffa['precio_6.1'] #+=margen_aplicado #dffm['precio_6.1']+margen
-#     dffa_copia['precio_2.0']+=margen_aplicado #=dffm['precio_2.0']+margen
-#     dffa_copia['precio_3.0']+=margen_aplicado #dffm['precio_3.0']+margen
-#     dffa_copia['precio_6.1']+=margen_aplicado #dffm['precio_6.1']+margen
-#     
-#     filtrar_mes(mes_seleccionado)
-# 
-#     return dffa_copia
+# %%
+def aplicar_margen(mes_seleccionado,margen_aplicado):
+    #dffa_copia=dffa
+    dffa_copia['precio_2.0']=dffa['precio_2.0'] #+=margen_aplicado #=dffm['precio_2.0']+margen
+    dffa_copia['precio_3.0']=dffa['precio_3.0'] #+=margen_aplicado #dffm['precio_3.0']+margen
+    dffa_copia['precio_6.1']=dffa['precio_6.1'] #+=margen_aplicado #dffm['precio_6.1']+margen
+    dffa_copia['precio_2.0']+=margen_aplicado #=dffm['precio_2.0']+margen
+    dffa_copia['precio_3.0']+=margen_aplicado #dffm['precio_3.0']+margen
+    dffa_copia['precio_6.1']+=margen_aplicado #dffm['precio_6.1']+margen
+    
+    filtrar_mes(mes_seleccionado)
 
-# %% [markdown]
-# ### De esta tabla sale el gráfico
-
-# %% [markdown]
-# ### Gráfico de salida para visualizar en streamlit
+    return dffa_copia
 
 
--- simulindex.py
+++ simulindex.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-#import globals
-
+import time
 from backend import graf_hist #max_reg, lista_meses, filtrar_mes, aplicar_margen, pt1_trans,graf_pt1, pt5_trans
-
-
 
 st.set_page_config(
     page_title="Simulindex",
@@ -18,49 +15,96 @@ st.set_page_config(
     )
 
 
-#elementos de la barra lateral
+# Crear un espacio vacío para el popup
 
+
+if 'margen' not in st.session_state:
+    margen=0
+
+if 'popup_mostrado' not in st.session_state:
+    st.session_state.popup_mostrado = False
+
+if not st.session_state.popup_mostrado:
+    popup = st.empty()
+    with popup.container():
+        st.markdown("""
+            <div style="background-color:#007BFF; color:white; padding: 20px; text-align: center; border-radius: 10px;">
+                <h2>¡Bienvenido a mis PowerAPPs!</h2>
+                <p>No di-simules, que te he pillao... 😎.</p>
+            </div>
+            """, unsafe_allow_html=True)
+
+    # Esperar 3 segundos
+    time.sleep(3)
+    
+    # Eliminar el popup después de 3 segundos
+    popup.empty()
+
+    # Marcar que el popup ya ha sido mostrado
+    st.session_state.popup_mostrado = True
+
+#ELEMENTOS DE LA BARRA LATERAL
 st.sidebar.subheader('Selecciona el valor de OMIP a un año vista')
-form1=st.sidebar.form('asdf')
-with form1:
-    omip = st.slider('OMIP en €/MWh',min_value=30,max_value=100,value=70, step=1)
-    st.form_submit_button('Recalcular')
-
-#exp=st.expander('Si quieres saber cómo funciona...')
-with st.sidebar.expander('Si quieres saber cómo funciona...'):
-    st.caption('Los :orange[puntos] son valores de indexado de los 12 últimos meses')
-    st.caption('Las :orange[líneas] reflejan una tendencia')
-    st.caption('Los :green[círculos] simulan los precios medios de indexado a un año vista en base a un valor de OMIP')
 
 
+with st.sidebar.container(border=True):
+    omip=st.slider(':green[OMIP] en €/MWh',min_value=30,max_value=100,value=70, step=1) #, key='omip_value',on_change=actualizar_grafico)
+    
+grafico, simul20, simul30, simul61 = graf_hist(omip)
 
+with st.sidebar.container(border=True):
+    añadir_margen=st.sidebar.toggle('Quieres añadir :violet[margen]?')
+    if añadir_margen:
+        margen=st.sidebar.slider('Añade margen al precio base de indexado en €/MWh', min_value=1,max_value=50, value=10, step=1)
+        #st.sidebar.slider('Añade margen al precio base de indexado en €/MWh', min_value=1,max_value=50, value=10, step=1,key='margen_value',on_change=change_margen)
+
+simul20_margen=simul20+margen/10
+simul30_margen=simul30+margen/10
+simul61_margen=simul61+margen/10
+
+st.sidebar.info('''
+        ¿Cómo funciona?        
+        \nLos :orange[puntos] son valores de indexado de los 12 últimos meses.  
+        \nLas :orange[líneas] reflejan una tendencia.  
+        \nLos :green[círculos] simulan los precios medios de indexado a un año vista en base a un valor de OMIP.
+    ''',icon="ℹ️")
 
 
 ## Layout de la página principal
-st.title("Simulindex 2024 webapp")
+st.title("Simulindex PowerAPP©")
 st.subheader("Tu aplicación para simular los futuros precios minoristas de indexado")
 st.caption("Copyright by Jose Vidal :ok_hand:")
 url_apps = "https://powerappspy-josevidal.streamlit.app/"
 st.write("Visita mi página de [PowerAPPs](%s) con un montón de utilidades" % url_apps)
-st.divider()
+url_linkedin = "https://www.linkedin.com/posts/jfvidalsierra_powerapps-activity-7216715360010461184-YhHj?utm_source=share&utm_medium=member_desktop"
+st.write("Deja tus comentarios y propuestas en mi perfil de [Linkedin](%s)" % url_linkedin)
+#st.divider()
 
 
-grafico,simul20,simul30,simul61=graf_hist(omip)
-#st.plotly_chart(grafico)
-
-col1,col2=st.columns([0.8,0.2])
+col1, col2 = st.columns([0.8, 0.2])
 with col1:
     st.plotly_chart(grafico)
-    
 with col2:
     with st.container(border=True):
-        st.subheader(':blue-background[Resultados]',divider='rainbow')
-        st.metric('OMIP de referencia €/MWh',value=omip,help='Este es el valor OMIP de referencia que has utilizado como entrada')
-        st.divider()
-        st.metric('Precio 2.0 c€/kWh',value=simul20, help='Este el precio 2.0 medio simulado a un año vista')
-        #with col12:
-        st.metric('Precio 3.0 c€/kWh',value=simul30,help='Este el precio 3.0 medio simulado a un año vista')
-        #with col13:
-        st.metric('Precio 6.1 c€/kWh',value=simul61,help='Este el precio 6.1 medio simulado a un año vista')
+        st.subheader(':blue-background[Datos de entrada]',divider='rainbow')
+        col11,col12=st.columns(2)
+        with col11:
+            st.metric(':green[OMIP] (€/MWh)',value=omip,help='Este es el valor OMIP de referencia que has utilizado como entrada')
+        with col12:
+            st.metric(':violet[Margen] (€/MWh)',value=margen,help='Margen que añades para obtener un precio medio final más ajustado a tus necesidades')
+    with st.container(border=True):
+        st.subheader(':green-background[Datos de salida]',divider='rainbow')
+        col13,col14=st.columns(2)
+        with col13:
+            st.text('Precios base')
+            st.metric(':orange[Precio 2.0] c€/kWh',value=simul20, help='Este el precio 2.0 medio simulado a un año vista')
+            st.metric(':red[Precio 3.0] c€/kWh',value=simul30,help='Este el precio 3.0 medio simulado a un año vista')
+            st.metric(':blue[Precio 6.1] c€/kWh',value=simul61,help='Este el precio 6.1 medio simulado a un año vista')
+        with col14:
+            st.text('Precios con margen')
+            st.metric(':orange[Precio 2.0] c€/kWh',value=round(simul20_margen,2), help='Este el precio 2.0 con el margen añadido')
+            st.metric(':red[Precio 3.0] c€/kWh',value=round(simul30_margen,2),help='Este el precio 3.0 con el margen añadido')
+            st.metric(':blue[Precio 6.1] c€/kWh',value=round(simul61_margen,2),help='Este el precio 6.1 con el margen añadido')
+            
 
 
