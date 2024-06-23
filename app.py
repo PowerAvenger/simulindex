f401303b09c96f63eaeeab4be0675cf11e0ee52d
Jspeedman <160780053+Jspeedman@users.noreply.github.com>
2024-06-23 20:26:32 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+from telemindex import graf_pt1
+
+st.set_page_config(layout='wide')
+st.title("Telemindex webapp")
+event=st.plotly_chart(graf_pt1)
+
